84de4d4bf6b4b05e4f6e051b6dbd565cb9a44699
刘可新 <liukexin08@maoyan.com>
2021-04-01 15:50:43 +0800

    vite初学习

---
+++ vite2/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1 class="name">lkx</h1>
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script lang="ts">
+import HelloWorld from '/@/components/HelloWorld.vue'
+import '/@/assets/app.css'
+import data from '/@/assets/lkx.json'
+const lkx: string = 'lkx~'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    console.log(lkx)
+    console.log(data.name + data.age)
+  }
+}
+</script>
+
+<style lang="scss">
+  .name{
+    color:green;
+  }
+</style>
---
+++ vite2/src/app.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      lkx~~~
+      <Child />
+    </div>
+  )
+}
+
+function Child() {
+  return (
+    <div>heheheheh</div>
+  )
+}
+
+export default App;
